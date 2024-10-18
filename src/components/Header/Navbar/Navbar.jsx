ba8a0f2eb7fc9e52dd82592d63d408ddb1b9d12a
Dylan Matthew Bragança <dmbraganca@gmail.com>
2024-10-18 09:50:51 -0600

    Fixed Navbar at bottom of page

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,29 +1,34 @@
-import './Navbar.css';
-import PropTypes from 'prop-types';
+'use client';
 
-Navbar.propTypes = {
-  options: PropTypes.arrayOf(
-    PropTypes.shape({
-      path: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-};
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
-function Navbar({ options = null }) {
-  return (
-    <nav data-testid="nav" className={'navbar'}>
-      <ul data-testid="ul">
-        {options?.map((optionNav, index) => (
-            <li key={index} data-testid={`li${index}`}>
-              <a data-testid={`a${index}`} className="link" href={optionNav.path}>
-                {optionNav.label}
-              </a>
-            </li>
-          ))}
-      </ul>
-    </nav>
-  );
+export function NavbarLink({href, label}) {
+    const pathname = usePathname();
+    const isActive = href === pathname;
+
+    return (
+        <li className="relative py-2 px-3 gap-x-1 text-base inline-flex flex-col">
+            <Link className={`${isActive ? 'text-teal-500 dark:text-teal-400' : 'text-zinc-800 dark:text-zinc-300'} hover:text-teal-500 dark:hover:text-teal-400 `} href={href}>{label}</Link>
+            {isActive &&
+                <span className="absolute float-start bottom-0 w-full h-[1px] bg-gradient-to-r from-transparent via-teal-500 to-transparent"></span>
+            }
+        </li>
+    )
 }
 
-export default Navbar;
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="flex flex-row items-center px-3 h-10 border border-zinc-900/5 dark:border-white-500/10 rounded-full">
+        <ul className="flex flex-row gap-x-2 items-center px-3">
+          <NavbarLink href="/" label="Home" />
+          <NavbarLink href="/about" label="About" />
+          <NavbarLink href="/projects" label="Projects" />
+          <NavbarLink href="/uses" label="Uses" />
+        </ul>
+      </nav>
+    </div>
+  );
+}
