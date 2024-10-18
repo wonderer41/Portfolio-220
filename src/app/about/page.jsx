a762b1d58627060580f4b83538c08130ac5a33ca
Jorge Vasquez <jmv254@ensign.edu>
2024-10-18 12:12:52 -0600

    left side - Jorge

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,17 +1,26 @@
+import Introduction from "@/components/Introduction/Introduction"
+
 export const metadata = {
   title: 'Portfolio - About',
   description: 'The about page of my portfolio.',
 }
 
-export default function About() {
+export default function About({title, content, socialLinks = []}) {
   return (
-    <div ClassName="flex justify-start items- ">
-      <div> 
-
-      </div>
-      <div>
-
+    <div className='flex justify-start items- '>
+      <Introduction ClassNametitle="" title={title} content={content}> 
+      <div className=''>
       </div>
     </div>
   );
-}
+  };
+  About.propTypes = {
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    socialLinks: PropTypes.arrayOf(
+        PropTypes.shape({
+            icon: PropTypes.string.isRequired,
+            link: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
