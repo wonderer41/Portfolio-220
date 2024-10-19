c65126dbe9c3190990813a76ef9ca98d15a58fc1
Gerardo Lazo <gerardoo.41@gmail.com>
2024-10-19 11:58:53 -0600

    update about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,6 @@
 import Introduction from "@/components/Introduction/Introduction";
 import PropTypes from "prop-types";
 import Image from "next/image";
-import { Roboto } from "next/font/google";
-const roboto = Roboto({
-  weight: ["400", "700"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export const metadata = {
   title: "Portfolio - About",
@@ -16,54 +9,83 @@ export const metadata = {
 
 export default function About() {
   return (
-    <div className="flex flex-col md:flex-row justify-between p-8 ">
-      <div className="md: w-1/2 mb-6 md:mb-0">
-        <h1 className="font-roboto font-bold text-5xl">
-          I’m Jane. I live in New York City, where I develop the future.
-        </h1>
-        <p className="font-roboto w-400 text-base mb-6">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, id exercitationem, amet fugiat facere, fugit
-          quam nihil laboriosam alias officiis nam. Eos aperiam consequatur officiis ipsa est explicabo quis ipsam.
-        </p>
-        <p className="font-roboto w-400 text-base mb-6">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, id exercitationem, amet fugiat facere, fugit
-          quam nihil laboriosam alias officiis nam. Eos aperiam consequatur officiis ipsa est explicabo quis ipsam.
-        </p>
-        <p className="font-roboto w-400 text-base mb-6">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, id exercitationem, amet fugiat facere, fugit
-          quam nihil laboriosam alias officiis nam. Eos aperiam consequatur officiis ipsa est explicabo quis ipsam.
-        </p>
-        <p className={`${roboto.className} font-roboto w-400 text-base mb-6`}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, id exercitationem, amet fugiat facere, fugit
-          quam nihil laboriosam alias officiis nam. Eos aperiam consequatur officiis ipsa est explicabo quis ipsam.
-        </p>
+    <div className="container mx-auto p-4">
+      <div className="flex flex-col md:flex-row justify-between gap-8">
+        {/* Left Section - Text */}
+        <div className="flex-1">
+          <h1 className="text-3xl font-bold mb-4">I'm Jane. I live in New York City, where I develop the future.</h1>
+          <p className="text-gray-600 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur
+            adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem
+            ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi..
+          </p>
+          <p className="text-gray-600 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur
+            adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem
+            ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi..
+          </p>
+          <p className="text-gray-600 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur
+            adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem
+            ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi..
+          </p>
+          <p className="text-gray-600 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur
+            adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem
+            ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi..
+          </p>
+        </div>
+
+        {/*rigth side*/}
+        <div className="flex-1  max-w-screen-lgshadow-168 rounded-xl h-auto overflow-hidden border border-gray-300">
+          {/*big image*/}
+          <div className="">
+            <Image
+              src="/about/aboutimage.jpeg"
+              alt="Profile picture"
+              className="w-full h-full object-cover"
+              width={464}
+              height={452}
+            />
+          </div>
+
+          {/*links*/}
+          <div className="p-6 text-center">
+            {/*twitter*/}
+            <a
+              href="https://twitter.com"
+              className="flex items-center justify-start mb-4 text-gray-700 hover:text-black"
+            >
+              <img src="/SocialLinks/twitter.svg" alt="Twitter" className="w-6 h-6 mr-2" />
+              <span>Follow me on X</span>
+            </a>
+
+            {/*git*/}
+            <a
+              href="https://github.com"
+              className="flex items-center justify-start mb-4 text-gray-700 hover:text-black"
+            >
+              <img src="/SocialLinks/github.svg" alt="Github" className="w-6 h-6 mr-2" />
+              <span>Follow me on Github</span>
+            </a>
+
+            {/* Lknd*/}
+            <a
+              href="https://linkedin.com"
+              className="flex items-center justify-start mb-4 text-gray-700 hover:text-black"
+            >
+              <img src="/SocialLinks/linkedin.svg" alt="LinkedIn" className="w-6 h-6 mr-2" />
+              <span>Follow me on LinkedIn</span>
+            </a>
+
+            {/*mail*/}
+            <div className="flex items-center justify-start text-gray-700">
+              <img src="/SocialLinks/Mail.png" alt="Email" className="w-6 h-6 mr-2" />
+              <span>jane@email.com</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="md:w-1/2 flex flex-col items-center">
-        <Image src="/about/aboutimage.jpeg" width={464} height={452} className="" alt="profile for about" />
-        <ul className="flex space-x-6">
-          <li>
-            <a href="http://www.w3.org/2000/svg"></a>
-            <menulink src="public\SocialLinks\twitter.svg" alt="twitter" />
-          </li>
-          <li>
-            <a href="http://www.w3.org/2000/svg"></a>
-            <img src="public\SocialLinks\github.svg" alt="git" />
-          </li>
-          <li>
-            <a href="http://www.w3.org/2000/svg"></a>
-            <menulink src="public\SocialLinks\linkedin.svg" alt="linkedin" />
-          </li>
-        </ul>
-      </div>
-      <ul>
-        <li>
-          <img src="public\SocialLinks\github.svg" alt="" />
-        </li>
-        <li>
-          <img src="public\SocialLinks\linkedin.svg" alt="" />
-        </li>
-        <li></li>
-      </ul>
     </div>
   );
 }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,28 +1,26 @@
-import {Roboto} from 'next/font/google';
-import Footer from '@/components/Footer/Footer';
-import Header from '@/components/Header/Header';
-import {footerLinks, navOptions} from '@/components/Data';
-import './globals.css';
+import { Roboto } from "next/font/google";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import { footerLinks, navOptions } from "@/components/Data";
+import "./globals.css";
 
 const roboto = Roboto({
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${roboto.className} bg-zinc-50 dark:bg-black lg:px-20 text-black dark:text-white`}>
-        <main className='flex flex-col justify-between min-h-screen bg-white dark:bg-zinc-900 max-w-7xl mx-auto'>
-        <Header navOptions={navOptions}/>
-          <div className="flex-grow">
-            {children}
-          </div>
-          <Footer links={footerLinks}/>
+        <main className="flex flex-col justify-between min-h-screen bg-white dark:bg-zinc-900 max-w-7xl mx-auto">
+          <Header navOptions={navOptions} />
+          <div className="flex-grow">{children}</div>
+          <Footer links={footerLinks} />
         </main>
       </body>
-  </html>
+    </html>
   );
 }
