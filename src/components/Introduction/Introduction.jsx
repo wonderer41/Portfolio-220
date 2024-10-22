f661663652d6b3fb3240acadccdcb11e047f9393
Dylan Matthew Bragança <dmbraganca@gmail.com>
2024-10-22 12:55:23 -0600

    Fixed introduction profile photo

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -4,7 +4,7 @@ import socialLinks from '../Data';
 const Introduction = ({title, content, socialLinks = []}) => {
     return (
         <div className='introduction flex flex-col gap-6' width={672} height={384}>
-            <Image src='/Introduction/profile.jpg' width={64} height={64} className="rounded-full border-2 border-inherit shadow-lg" alt='Profile picture'/>
+            <Image src='/Introduction/profile.jpg' width={64} height={64} className="rounded-full border-2 border-white/90 dark:border-zinc-800/90 shadow-lg" alt='Profile picture'/>
             <h1 className="w-[672px] h-[112px] font-roboto font-bold text-5xl leading-14">{title}</h1>
             <p className="w-[672px] h-[112px] font-roboto text-base leading-7">{content}</p>
             <ul className='flex flex-row gap-3'>
