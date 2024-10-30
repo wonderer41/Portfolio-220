e418102508ab2eb5f5dda33cbd7674e980180832
Jorge Vasquez <jmv254@ensign.edu>
2024-10-29 19:41:06 -0600

    uses section border

--- src/components/UsesSection/UsesSection.jsx
+++ src/components/UsesSection/UsesSection.jsx
@@ -2,8 +2,8 @@ import UseCard from "@/components/UseCard/UsesCard";
 
 const UsesSection = ({ groupName, items = [] }) => {
   return (
-    <section className="flex flex-col p-0 lg:px-[15px] lg:border-zink-700/40 lg:border-l lg:gap-20">
-      <div className="flex flex-col lg:flex-row justify-between lg:pl-[24px] border-l-1 border-zinc-100">
+    <section className="flex flex-col p-0 lg:px-[15px] ">
+      <div className="flex flex-col lg:flex-row justify-between lg:pl-[24px] lg:border-zink-400 lg:border-l lg:gap-20">
         <h2>{groupName}</h2>
         <div className="max-w-[738px] flex flex-col gap-y-[32px]">
           {items.map((subitem, subIndex) => (
@@ -11,7 +11,6 @@ const UsesSection = ({ groupName, items = [] }) => {
           ))}
         </div>
       </div>
-      
     </section>
   );
 };
